f29c5aaef185644b47d5717fe00680f8dd796c3f
ASHISH JAIN <ashishbohara1008@gmail.com>
2019-12-15 22:18:27 +0530

    i tired

--- app/Http/Middleware/AuthGates.php
+++ app/Http/Middleware/AuthGates.php
@@ -6,11 +6,11 @@ use App\Admin;
 use App\Permission;
 use Closure;
 use Illuminate\Support\Facades\Gate;
-use App\Http\Middleware\CheckAuth;
 class AuthGates
 {
     public function handle($request, Closure $next)
     {
+        // config(['auth.defaults.guard' => 'admins']);
         $admin_id = $request->session()->get('admin_id');
 
         if (!app()->runningInConsole() && $admin_id) {
@@ -18,14 +18,26 @@ class AuthGates
             $permissions = Permission::all();
             $admin->load('permissions');
 
-            // foreach ($admin->permissions as $permissions) {
-            //     $permissionsArray[$permissions->title][] = $admin->id;
-            // }
+            foreach ($admin->permissions as $permissions) {
+                $permissionsArray[$permissions->title][] = $admin->id;
+            }
             // dd($permissionsArray);
 
-            foreach ($admin->permissions as $permission) {
-                // dd($permission->title);
-                Gate::define($permission->title, 'true');
+            // foreach ($admin->permissions as $permission) {
+            //     dd(Gate::define($permission->title, 'true'));
+            //     Gate::define($permission->title, 'true');
+            // }
+            foreach ($permissionsArray as $title => $admins) {
+                // dd( Gate::define($title, function (\App\Admin $admin) use ($admins) {
+                //     return count(array_intersect($admin->pluck('id')->toArray(), $admins)) > 0;
+                // }));
+                // $aa = Admin::pluck('id')->toArray();
+                // dd(count(array_intersect($aa, $admins)) > 0);
+                // dd($aa);
+                Gate::define($title, function (\App\Admin $admin) use ($admins) {
+                    //dd($admin->pluck('id'));
+                    return count(array_intersect($admin->pluck('id')->toArray(), $admins)) > 0;
+                });
             }
         }
 
