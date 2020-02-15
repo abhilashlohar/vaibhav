4aa5f22a4f537eb89c29f9fbdcc924a8b01a17a8
ASHISH JAIN <ashishbohara1008@gmail.com>
2020-02-16 00:23:14 +0530

    working on home page

---
+++ app/Http/Controllers/admin/HomePageProductController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\admin;
+
+use App\HomePageProduct;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class HomePageProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\HomePageProduct  $homePageProduct
+     * @return \Illuminate\Http\Response
+     */
+    public function show(HomePageProduct $homePageProduct)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\HomePageProduct  $homePageProduct
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(HomePageProduct $homePageProduct)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\HomePageProduct  $homePageProduct
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, HomePageProduct $homePageProduct)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\HomePageProduct  $homePageProduct
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(HomePageProduct $homePageProduct)
+    {
+        //
+    }
+}
